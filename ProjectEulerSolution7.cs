d627f760d1e6b9bc6c5900325d68dd6fe7bf723e
ibharadia <ibha2048@gmail.com>
2016-09-05 23:36:47 -0400

    Solution to problem 7
    C# console app

---
+++ ProjectEulerSolution7.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler7
+{
+    class ProjectEulerSolution7
+    {
+        static void Main(string[] args)
+        {
+            var possiblePrime = 2;
+            var thisPrime = 0;
+            var numOfPrimes = 0;
+            while (numOfPrimes < 10001)
+            {
+                thisPrime = PrimeNums(possiblePrime);
+                if (thisPrime > 0)
+                {
+                    numOfPrimes++;
+                    Console.WriteLine("Prime number " +numOfPrimes +" is: " +thisPrime);
+                }
+                possiblePrime++;
+            }
+            var quit = Console.ReadLine();
+        }
+
+
+        public static int PrimeNums(int x)
+        {
+            var returningNumber = 0;
+            for (var i = 1; i < x; i++)
+            {
+                if ((x%i == 0) && (i != x) && (i != 1))
+                {
+                    returningNumber =  0;
+                    break;
+                }
+                else
+                {
+                    returningNumber = x;
+                }
+            }
+            return returningNumber;
+        }
+    }
+}
